6df6019497b4bacf81c746aaab2a8997224b23f9
MustufaMemon10 <mustufasam06@gmail.com>
2024-07-11 14:55:13 +0530

    make desired Nav bar

--- lib/Navigation.dart
+++ lib/Navigation.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hiremi_profile/Nav_Bar.dart';
 import 'package:hiremi_profile/screens/Verified_Profile_Section/ProfileScreen.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+
+import 'core/utils/constants/AppSizes.dart';
+import 'core/utils/constants/colors.dart';
+
 
 class NewNavbar extends StatefulWidget {
   final bool isV;
+
   const NewNavbar({super.key, required this.isV});
 
   @override
@@ -15,14 +20,15 @@ class _NewNavbarState extends State<NewNavbar> {
   final PageController _pageController = PageController();
 
   late List<Widget> _pages;
+
   @override
   void initState() {
     super.initState();
     _pages = [
-        ProfileScreen(),
-       ProfileScreen(),
-       ProfileScreen(),
-       ProfileScreen(),
+      ProfileScreen(),
+      ProfileScreen(),
+      ProfileScreen(),
+      ProfileScreen(),
     ];
   }
 
@@ -35,8 +41,6 @@ class _NewNavbarState extends State<NewNavbar> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double spacing = (screenWidth - (4 * 50)) / 5;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -44,13 +48,14 @@ class _NewNavbarState extends State<NewNavbar> {
           PageView(
             controller: _pageController,
             children: _pages
-                .map((page) => Navigator(
-              onGenerateRoute: (settings) {
-                return MaterialPageRoute(
-                  builder: (context) => page,
-                );
-              },
-            ))
+                .map((page) =>
+                Navigator(
+                  onGenerateRoute: (settings) {
+                    return MaterialPageRoute(
+                      builder: (context) => page,
+                    );
+                  },
+                ))
                 .toList(),
             onPageChanged: (index) {
               setState(() {
@@ -58,116 +63,38 @@ class _NewNavbarState extends State<NewNavbar> {
               });
             },
           ),
-          Positioned(
-            bottom: 10,
-            left: 20,
-            right: 20,
-            child: Container(
-              height: 64,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  topRight: Radius.circular(16),
-                  bottomLeft: Radius.circular(32),
-                  bottomRight: Radius.circular(32),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 10,
-                    offset: Offset(0, 5),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Row(
+    ]
+      ),
+          bottomNavigationBar: const BottomNavBar(),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton:
+          FloatingActionButton(
+              onPressed: () {
+                // Define the action for the FloatingActionButton here
+              },
+              elevation: 0,
+              backgroundColor: Colors.white,
+              shape: const CircleBorder(),
+              child:  const Center(
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    _buildNavItem(Icons.home_filled, 'HOME', 0),
-                    SizedBox(
-                      width: spacing,
+                    Icon(
+                      Icons.all_inclusive,
+                      color: Color(0xFFC1272D),
+                      size: 20,
                     ),
-                    _buildNavItem(Icons.file_copy, 'APPLIES', 1),
-                    SizedBox(
-                      width: spacing * 3.2,
+                    Text(
+                      'HIREMI',
+                      style: TextStyle(fontSize: 7, fontWeight: FontWeight.bold),
                     ),
-                    _buildNavItem(Icons.chat, 'QUERIES', 2),
-                    SizedBox(
-                      width: spacing,
+                    Text(
+                      '360',
+                      style: TextStyle(fontSize: 6, color: Color(0xFFC1272D)),
                     ),
-                    _buildNavItem(Icons.person, 'PROFILE', 3),
                   ],
                 ),
-              ),
-            ),
-          ),
-          // Positioned(
-          //     bottom:
-          //         10,
-          //     left: 0,
-          //     right: 0,
-          //     child: SizedBox(height: 100, width: 5,child: Image.asset('assets/Rectangle 50.png'))
-          //     ),
-          Positioned(
-              bottom: 35,
-              left: screenWidth*0.405,
-              child: CircularPercentIndicator(
-                radius: 39,
-                lineWidth: 15,
-                percent: 0.50,
-
-                progressColor: const Color(0xFFC1272D),
-                backgroundColor: Colors.transparent,
-                startAngle: 90,
               )),
-          Positioned(
-            bottom: 25,
-            left: 0,
-            right: 0,
-            child: Transform.translate(
-              offset: const Offset(0, -20),
-              child: Center(
-                child: SizedBox(
-                  width: 64,
-                  height: 64,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      // Define the action for the FloatingActionButton here
-                    },
-                    backgroundColor: Colors.white,
-                    shape: const CircleBorder(),
-                    child: const Center(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Icon(
-                            Icons.all_inclusive,
-                            color: Color(0xFFC1272D),
-                            size: 30,
-                          ),
-                          Text(
-                            'HIREMI',
-                            style: TextStyle(
-                                fontSize: 7, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            '360',
-                            style: TextStyle(
-                                fontSize: 6, color: Color(0xFFC1272D)),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 
@@ -182,7 +109,7 @@ class _NewNavbarState extends State<NewNavbar> {
             icon,
             size: 20,
             color: _selectedIndex == index
-                ? const Color(0xFFC1272D)
+                ? AppColors.primary
                 : Colors.black,
           ),
           Text(
